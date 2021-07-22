92d68280bd08058e839557baa5086475c6f25001
Alexander Imra <avimra@gmail.com>
2021-07-22 14:58:49 -0700

    Minor fixes

--- src/bindingHandlers/copyToClipboard.ts
+++ src/bindingHandlers/copyToClipboard.ts
@@ -3,21 +3,11 @@ import * as ko from "knockout";
 ko.bindingHandlers["copyToClipboard"] = {
     init: (element: HTMLElement, valueAccessor: () => string): void => {
         const copyToClipboard = () => {
-            const placeholder = document.createElement("textarea");
-            placeholder.innerText = ko.unwrap(valueAccessor());
-            document.body.appendChild(placeholder);
-
-            const range = document.createRange();
-            range.selectNode(placeholder);
-
-            const selection = window.getSelection();
-            selection.removeAllRanges();
-            selection.addRange(range);
-
-            document.execCommand("copy");
-
-            selection.removeAllRanges();
-            document.body.removeChild(placeholder);
+            try {
+                const text = ko.unwrap(valueAccessor());
+                navigator.clipboard.writeText(text).catch(e => { });
+            }
+            catch (e) { }
         };
 
         ko.applyBindingsToNode(element, { click: copyToClipboard }, null);
