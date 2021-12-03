24961ad8330d99a9dc7cd95c8d24dfcf88b61981
Alexander Imra <avimra@gmail.com>
2021-12-03 15:18:38 -0800

    Hide endpoint

--- src/components/operations/operation-list/ko/runtime/operation-list.ts
+++ src/components/operations/operation-list/ko/runtime/operation-list.ts
@@ -192,7 +192,9 @@ export class OperationList {
                 .replace('/tenant/{tenant}/gps-partner/', '')
                 .replace('/tenant/', '')
             );
-            g.items.sort((a, b) => a.urlTemplate > b.urlTemplate ? 1 : -1);
+            g.items =g.items
+                .filter(i => i.urlTemplate != '{tenant}/jobs/{id}/hold')
+                .sort((a, b) => a.urlTemplate > b.urlTemplate ? 1 : -1);
         });
 
         this.operationGroups(operationGroups);
