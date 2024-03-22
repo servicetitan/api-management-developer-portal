2d75db13da8c9d2868b078e1df22d2845c9d05f3
Michael Liu <mliu@servicetitan.com>
2024-03-22 16:35:05 -0700

    API-2024: Add API Usage Report form

--- community/widgets/api-apps/ko/runtime/apiAppApiUsageReportVm.ts
+++ community/widgets/api-apps/ko/runtime/apiAppApiUsageReportVm.ts
@@ -2,8 +2,16 @@ import * as ko from "knockout";
 import { ApiAppContract } from "../../services/apiAppContract";
 import { ApiAppsService } from "../../services/apiAppsService";
 
-function parseYYYYMMDD(date: string): Date {
-    return new Date(+date.slice(0, 4), +date.slice(5, 7) - 1, +date.slice(8, 10));
+// Attempts to parse a user-entered date in the format MM/DD/YYYY (with - and . also allowed as separators)
+function parseDate(value: string): Date {
+    const match = value.match(/^\s*(\d{1,2})\s*[-./]\s*(\d{1,2})\s*[-./]\s*(\d{4})\s*$/);
+    if (match) {
+        value =`${match[3]}-${("0" + match[1]).slice(-2)}-${("0" + match[2]).slice(-2)}`; // YYYY-MM-DD
+        const date = new Date(value); // midnight UTC
+        if (!isNaN(+date) && date.toISOString().slice(0, 10) === value) // date is valid and wasn't adjusted?
+            return new Date(`${value}T00:00:00`); // midnight local
+    }
+    return new Date(NaN); // invalid date
 }
 
 export class ApiAppApiUsageReportVm {
@@ -12,6 +20,14 @@ export class ApiAppApiUsageReportVm {
     public name: string;
     public createdOn: Date;
     public minDate: Date;
+    public startDate: ko.Observable<string>;
+    public startDateParsed: ko.PureComputed<Date>;
+    public startDateValidation: ko.PureComputed<string>;
+    public endDate: ko.Observable<string>;
+    public endDateParsed: ko.PureComputed<Date>;
+    public endDateValidation: ko.PureComputed<string>;
+    public validationActivated: ko.Observable<boolean>;
+    public isValid: ko.PureComputed<boolean>;
 
     constructor(
         private apiAppsService: ApiAppsService,
@@ -22,11 +38,43 @@ export class ApiAppApiUsageReportVm {
         this.id = apiApp.id;
         this.publicId = apiApp.publicId;
         this.name = apiApp.name;
-        this.createdOn = parseYYYYMMDD(apiApp.createdOn);
-        this.minDate = parseYYYYMMDD(apiApp.apiUsageReportMinDate);
+        this.createdOn = new Date(apiApp.createdOn);
+        this.minDate = new Date(apiApp.apiUsageReportMinDate);
+        this.startDate = ko.observable("");
+        this.startDateParsed = ko.pureComputed(() => parseDate(this.startDate()));
+        this.startDateValidation = ko.pureComputed(() => {
+            if (!this.validationActivated()) return "";
+            const startDate = this.startDateParsed();
+            if (isNaN(+startDate)) return "Enter a valid date (MM/DD/YYYY).";
+            if (startDate < this.minDate)
+                return `The Start Date cannot be earlier than ${this.minDate.toLocaleDateString()}.`;
+            return "";
+        });
+        this.endDate = ko.observable("");
+        this.endDateParsed = ko.pureComputed(() => parseDate(this.endDate()));
+        this.endDateValidation = ko.pureComputed(() => {
+            if (!this.validationActivated()) return "";
+            const endDate = this.endDateParsed();
+            if (isNaN(+endDate)) return "Enter a valid date (MM/DD/YYYY).";
+            if (endDate < this.startDateParsed())
+                return "The End Date cannot be earlier than the Start Date.";
+            return "";
+        });
+        this.validationActivated = ko.observable(false);
+        this.isValid = ko.pureComputed(() => 
+            this.startDateValidation() === "" &&
+            this.endDateValidation() === ""
+        );
     }
 
     public async clickBack() {
         await this.close();
+    }
+
+    public async clickDownload() {
+        this.validationActivated(true);
+        if (!this.isValid()) {
+            return;
+        }
     }
 }
